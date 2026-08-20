520fbbe4018c82c404ce4c00613b78a58280d1f2
Felipe Ricardo <felipeeemoreiraaa12@gmail.com>
2026-08-20 08:57:37 -0300

    feat:Componente utilizando props

---
+++ src/components/Aluno.jsx
@@ -0,0 +1,15 @@
+
+import Aluno from './components/'
+
+const Aluno = (props) => {
+    return (
+        <>
+            <p>O nome do aluno é:(props.nome)</p>
+            <p>A idade do aluno é:(props.idade)</p>
+        </>
+        
+
+    )
+}
+
+export default App
